1f6328871a06e6c78de3069e8f4fe49eda057270
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-02-04 03:22:09 +0100

    initial commit

---
+++ src/components/my-first-component.tsx
@@ -0,0 +1,12 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'my-first-component',
+})
+export class myComponent {
+  @Prop() name: string;
+
+  render() {
+    return <p>My name is {this.name}</p>;
+  }
+}
